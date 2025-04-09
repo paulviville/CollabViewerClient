fd0e8a349abd1ff0a92e90d678101d5d0d83e6f3
paulviville <paul.viville@gmail.com>
2025-04-09 23:14:56 +0200

    synchronization with pointers

--- main.js
+++ main.js
@@ -13,73 +13,71 @@ const sceneInterface = new SceneInterface();
 
 
 
-// const scene = new THREE.Scene();
-// scene.background = new THREE.Color(0x555555);
-
-// let ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-// scene.add(ambientLight);
-// let pointLight0 = new THREE.PointLight(0xffffff, 100);
-// pointLight0.position.set(5,4,5);
-// scene.add(pointLight0);
-
 const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 50 );
 camera.position.set( 0, 1.6, 2.5 );
-
-// const renderer = new THREE.WebGLRenderer({antialias: true});
-// renderer.autoClear = false;
-// renderer.setPixelRatio( window.devicePixelRatio );
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
-
-// const orbitControls = new OrbitControls(camera, renderer.domElement);
 
 
 const otherCameras = {};
 const otherCamerasHelpers = {};
+const otherPointers = {};
+const otherPointersHelpers = {};
 
 
 let testId = 123456;
-// otherCameras[testId] = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.01, 1 );
-// console.log(otherCameras[testId])
-// otherCamerasHelpers[testId] = new THREE.CameraHelper(otherCameras[testId]);
-// scene.add(otherCamerasHelpers[testId])
-// console.log(camera)
 
 
 const grid = new THREE.GridHelper(20, 20)
-scene.add(grid)
+sceneInterface.scene.add(grid)
 
-
-const testCamera = otherCameras[testId]
-const testCameraHelper = otherCamerasHelpers[testId]
 
 
 
 function addCamera(id) {
-  otherCameras[id] = new THREE.PerspectiveCamera( 50, 16/9, 0.01, 1 );
-  otherCamerasHelpers[id] = new THREE.CameraHelper(otherCameras[id]);
-  scene.add(otherCamerasHelpers[id]);
+	otherCameras[id] = new THREE.PerspectiveCamera( 50, 16/9, 0.01, 1 );
+	otherCamerasHelpers[id] = new THREE.CameraHelper(otherCameras[id]);
+	sceneInterface.scene.add(otherCamerasHelpers[id]);
 }
 
 function removeCamera(id) {
-  scene.remove(otherCamerasHelpers[id]);
-  otherCamerasHelpers[id].dispose()
-  delete(otherCameras[id]);
-  delete(otherCamerasHelpers[id]);
+	sceneInterface.scene.remove(otherCamerasHelpers[id]);
+  	otherCamerasHelpers[id].dispose()
+	delete(otherCameras[id]);
+	delete(otherCamerasHelpers[id]);
 }
 
+const sphereGeometry = new THREE.SphereGeometry(0.1, 10, 10);
+const sphereMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00, wireframe: true});
+function addPointer(id) {
+	otherPointers[id] = {p0: new THREE.Vector3(), p1: new THREE.Vector3()};
 
+	const sphereMesh0 = new THREE.Mesh(sphereGeometry, sphereMaterial);
+	const sphereMesh1 = new THREE.Mesh(sphereGeometry, sphereMaterial);
+	otherPointersHelpers[id] = {p0: sphereMesh0, p1: sphereMesh1};
+	sceneInterface.scene.add(otherPointersHelpers[id].p0, otherPointersHelpers[id].p1);
+}
 
+function removePointer(id) {
+	sceneInterface.scene.remove(otherPointersHelpers[id].p0, otherPointersHelpers[id].p1);
+	delete(otherPointers[id]);
+	delete(otherPointersHelpers[id]);
+}
 
+const sphereMaterial2 = new THREE.MeshBasicMaterial({color: 0xFF0000, wireframe: true});
+const pointer = {p0: new THREE.Vector3(), p1: new THREE.Vector3()};
+const pointerHelper = {p0:  new THREE.Mesh(sphereGeometry, sphereMaterial2), p1:  new THREE.Mesh(sphereGeometry, sphereMaterial2)};
 
+sceneInterface.scene.add(pointerHelper.p0, pointerHelper.p1);
 
 
 
 const SERVER_ID = 0;
 
-const NEW_PLAYER = 0;
-const REMOVE_PLAYER = 1;
-const UPDATE_CAMERA = 2;
+
+let macroID = 0;
+const NEW_PLAYER = macroID++;
+const REMOVE_PLAYER = macroID++;
+const UPDATE_CAMERA = macroID++;
+const UPDATE_POINTER = macroID++;
 
 
 let clientId = 0;
@@ -107,6 +105,7 @@ socket.onmessage = function(event) {
         if(clientId == 0){
           clientId = data[2];
           console.log(`Player id initialized ${data[2]}`);
+		  sendCameraData();
         } 
         /// get other ids
         else {
@@ -116,6 +115,7 @@ socket.onmessage = function(event) {
           console.log(otherClientsId)
 
           addCamera(data[2]);
+          addPointer(data[2]);
         }
         break;
       case REMOVE_PLAYER:
@@ -123,6 +123,7 @@ socket.onmessage = function(event) {
         delete(otherClientsId[data[2]])
         console.log(`Player ${data[2]} disconnected`);
         removeCamera(data[2]);
+        removePointer(data[2]);
         break;
       default:
         break;
@@ -133,37 +134,75 @@ socket.onmessage = function(event) {
   else {
     switch(commandId){
       case UPDATE_CAMERA:
-        // console.log("update camera")
-        otherCameras[senderId].position.set(data[2], data[3], data[4]);
-        otherCameras[senderId].rotation.set(data[5], data[6], data[7]);
-        otherCameras[senderId].updateMatrixWorld()
-        otherCamerasHelpers[senderId].update()
+		console.log("UPDATE_CAMERA")
+		updateCamera(senderId, {x: data[2], y: data[3], z: data[4]}, {x: data[5], y: data[6], z: data[7]});
+		break;
+	case UPDATE_POINTER:
+		updatePointer(senderId, {x: data[2], y: data[3], z: data[4]}, {x: data[5], y: data[6], z: data[7]});
+		break;
     }
     // console.log(data)
   }
 };
 
 
-function sendMessage(text) {
-  socket.send(text);
+function updatePointer ( id, p0, p1 ) {
+	otherPointers[id].p0.set(p0.x, p0.y, p0.z);
+	otherPointers[id].p1.set(p1.x, p1.y, p1.z);
+
+	otherPointersHelpers[id].p0.position.copy(otherPointers[id].p0);
+	otherPointersHelpers[id].p1.position.copy(otherPointers[id].p1);
 }
 
-function sendCameraData() {
-  const cameraData = new Float32Array([
-    clientId,
-    UPDATE_CAMERA,
-    camera.position.x,
-    camera.position.y,
-    camera.position.z,
-    camera.rotation.x,
-    camera.rotation.y,
-    camera.rotation.z
-  ]);
-  socket.send(cameraData);
+function updateCamera( id, position, rotation ) {
+	otherCameras[id].position.set(position.x, position.y, position.z);
+	otherCameras[id].rotation.set(rotation.x, rotation.y, rotation.z);
+	otherCameras[id].updateMatrixWorld();
+	otherCamerasHelpers[id].update();
 }
 
+function sendMessage ( text ) {
+	socket.send(text);
+}
+
+function sendCameraData ( ) {
+	const cameraData = new Float32Array([
+		clientId,
+		UPDATE_CAMERA,
+		sceneInterface.camera.position.x,
+		sceneInterface.camera.position.y,
+		sceneInterface.camera.position.z,
+		sceneInterface.camera.rotation.x,
+		sceneInterface.camera.rotation.y,
+		sceneInterface.camera.rotation.z
+	]);
+	socket.send(cameraData);
+}
+
+function sendPointer ( ) {
+	const {p0, p1} = sceneInterface.pointer;
+	console.log(p0, p1);
+	pointer.p0.copy(p0);
+	pointer.p1.copy(p1);
+
+	pointerHelper.p0.position.copy(pointer.p0);
+	pointerHelper.p1.position.copy(pointer.p1);
+
+	const pointerData = new Float32Array([
+		clientId,
+		UPDATE_POINTER,
+		pointer.p0.x,
+		pointer.p0.y,
+		pointer.p0.z,
+		pointer.p1.x,
+		pointer.p1.y,
+		pointer.p1.z,
+	]);
+	socket.send(pointerData);
+}
 
 window.sendMessage = sendMessage;
+window.sendPointer = sendPointer;
 window.sendCameraData = sendCameraData;
 
 
@@ -171,11 +210,18 @@ window.sendCameraData = sendCameraData;
 
 
 function animate() {
-  if(nbClients)
-    sendCameraData()
-  // renderer.render( scene, camera );
 
-  sceneInterface.renderer.render( sceneInterface.scene, sceneInterface.camera );
+
+	sceneInterface.renderer.render( sceneInterface.scene, sceneInterface.camera );
+
+	if( nbClients == 0 )
+		return;
+
+	if( sceneInterface.cameraNeedsUpdate )
+		sendCameraData();
+
+	if( sceneInterface.pointerNeedsUpdate )
+		sendPointer();
 }
 
-renderer.setAnimationLoop( animate );
+sceneInterface.renderer.setAnimationLoop( animate );
